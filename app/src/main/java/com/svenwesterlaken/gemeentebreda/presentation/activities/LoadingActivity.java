f46feb3f9317c46817c7327fccbfa2eb822f9d95
Sven Westerlaken <sven.westerlaken@hotmail.nl>
2017-05-03 19:25:34 +0200

    Project Setup

---
+++ app/src/main/java/com/svenwesterlaken/gemeentebreda/presentation/activities/LoadingActivity.java
@@ -0,0 +1,23 @@
+package com.svenwesterlaken.gemeentebreda.presentation.activities;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.svenwesterlaken.gemeentebreda.R;
+
+public class LoadingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loading);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+}
---
+++ app/src/main/java/com/svenwesterlaken/gemeentebreda/presentation/fragments/ReportMapFragment.java
@@ -0,0 +1,10 @@
+package com.svenwesterlaken.gemeentebreda.presentation.fragments;
+
+import android.app.Fragment;
+
+/**
+ * Created by Sven Westerlaken on 27-4-2017.
+ */
+
+public class ReportMapFragment extends Fragment {
+}
