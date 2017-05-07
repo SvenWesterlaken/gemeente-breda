51b42e9fa7ca6f525c7499a87ca5a7d36b838922
lukamarijn <lukabrinkmannn@gmail.com>
2017-05-07 14:25:24 +0200

    Tabs toegevoegd

--- app/src/main/java/com/svenwesterlaken/gemeentebreda/presentation/fragments/ReportMapFragment.java
+++ app/src/main/java/com/svenwesterlaken/gemeentebreda/presentation/fragments/ReportMapFragment.java
@@ -1,10 +1,18 @@
 package com.svenwesterlaken.gemeentebreda.presentation.fragments;
 
-import android.app.Fragment;
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.svenwesterlaken.gemeentebreda.R;
 
 /**
  * Created by Sven Westerlaken on 27-4-2017.
  */
 
 public class ReportMapFragment extends Fragment {
+
+
 }
---
+++ app/src/main/java/com/svenwesterlaken/gemeentebreda/presentation/fragments/ReportTab2.java
@@ -0,0 +1,20 @@
+package com.svenwesterlaken.gemeentebreda.presentation.fragments;
+
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.svenwesterlaken.gemeentebreda.R;
+
+public class ReportTab2 extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        View rootView = inflater.inflate(R.layout.activity_report_tab2, container, false);
+        return rootView;
+    }
+}
