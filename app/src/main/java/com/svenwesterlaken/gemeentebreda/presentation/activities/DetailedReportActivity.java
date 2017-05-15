bb06568045aff60bfc34ace14d537951f8e5012b
lukamarijn <lukabrinkmannn@gmail.com>
2017-05-15 21:57:36 +0200

    Start layout detailed report

---
+++ app/src/main/java/com/svenwesterlaken/gemeentebreda/presentation/activities/DetailedReportActivity.java
@@ -0,0 +1,16 @@
+package com.svenwesterlaken.gemeentebreda.presentation.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+
+import com.svenwesterlaken.gemeentebreda.R;
+
+public class DetailedReportActivity extends MenuActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detailed_report);
+    }
+}
