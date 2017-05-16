dbc346571e48955908ca65941f6ddfd84ae665d1
Postmeneer420 <rick2610@live.nl>
2017-05-16 21:29:00 +0200

    EditText Report toegevoegd
    1 regel in de databasehandler uitgecomment
    
    ja dit is nederlands kom vechten sven

--- app/src/main/java/com/svenwesterlaken/gemeentebreda/presentation/fragments/NewReportDescriptionFragment.java
+++ app/src/main/java/com/svenwesterlaken/gemeentebreda/presentation/fragments/NewReportDescriptionFragment.java
@@ -1,17 +1,22 @@
 package com.svenwesterlaken.gemeentebreda.presentation.fragments;
 
-import android.content.Context;
-import android.net.Uri;
-import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
+        import android.content.Context;
+        import android.net.Uri;
+        import android.os.Bundle;
+        import android.support.v4.app.Fragment;
+        import android.support.v7.app.AppCompatActivity;
+        import android.view.LayoutInflater;
+        import android.view.View;
+        import android.view.ViewGroup;
+        import android.widget.EditText;
 
-import com.svenwesterlaken.gemeentebreda.R;
+        import com.svenwesterlaken.gemeentebreda.R;
+        import com.svenwesterlaken.gemeentebreda.domain.Report;
 
 
-public class NewReportDescriptionFragment extends Fragment {
+public class  NewReportDescriptionFragment extends Fragment{
+
+    EditText report;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -20,6 +25,14 @@ public class NewReportDescriptionFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_new_report_description, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_new_report_description, container, false);
+
+        report = (EditText) rootView.findViewById(R.id.reportText);
+
+        Report r = new Report();
+
+        r.setReport(report.getText().toString());
+
+        return rootView;
     }
 }
