a1cd759c7a85bc8e23d79c0a4976a49fdf0a6a9d
Sven Westerlaken <sven.westerlaken@hotmail.nl>
2017-05-21 22:40:10 +0200

    Fixed database error. Added confirmation screen & added back the functionallity to add random reports.

---
+++ app/src/main/java/com/svenwesterlaken/gemeentebreda/presentation/partials/NotImplementedListener.java
@@ -0,0 +1,33 @@
+package com.svenwesterlaken.gemeentebreda.presentation.partials;
+
+import android.content.Context;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+/**
+ * Created by Sven Westerlaken on 21-5-2017.
+ */
+
+public class NotImplementedListener implements MenuItem.OnMenuItemClickListener, View.OnClickListener {
+
+    private Context context;
+    private Toast toast;
+
+    public NotImplementedListener(Context c) {
+        this.context = c;
+        toast = Toast.makeText(context, "Deze functie is nog niet geïmplementeerd", Toast.LENGTH_SHORT);
+    }
+
+    @Override
+    public boolean onMenuItemClick(MenuItem item) {
+        toast.show();
+        return true;
+    }
+
+    @Override
+    public void onClick(View v) {
+        toast.show();
+    }
+}
